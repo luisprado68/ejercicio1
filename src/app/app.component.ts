a0dfa2c5728c012551efe987ac04e1844ef1ba55
Luis Prado <luisprado0095@gmail.com>
2021-03-22 23:15:45 -0300

    Ejercicio 1 realizado :fire:

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ejercicio';
+  
+  title:string = 'Ejercicio 1';
+  suma:number=0;
+  promedio:number=0;
+  numero:number=0;
+  numeroDos:number=0;
+
+   
+   calcular():void{
+
+    this.suma =this.numero + this.numeroDos;
+    this.promedio = this.suma / 2;
+   }
+
+   limpiar():void{
+      this.numero=0;
+      this.numeroDos=0;
+      this.suma=0;
+      this.promedio=0;
+   }
+
 }
